8a6aaa71719f411cb7f0e7f576de9861728a9e31
Daniel <danleavitt0@gmail.com>
2016-10-06 20:29:40 -0700

    remove panda

--- src/animalApis/animalDescriptions.js
+++ src/animalApis/animalDescriptions.js
@@ -2,9 +2,5 @@ export default {
   zebra: {
     description: 'The zebra uses only simple directional movements and takes no arguments. It can only paint in black.',
     image: '/animalImages/zebra.jpg'
-  },
-  panda: {
-    description: 'The panda can use numerical arguments to move multiple steps at a time. The panda can only paint black.',
-    image: '/animalImages/panda.png'
   }
 }
