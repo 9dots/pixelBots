95cb4920a2cfda5b5cbe42e3c2069d762d0b44a0
Daniel <danleavitt0@gmail.com>
2016-11-09 10:50:48 -0800

    saving

---
+++ src/middleware/saveCode.js
@@ -0,0 +1,24 @@
+import {addCode, removeLine, updateLine, aceUpdate, saveProgress} from '../actions'
+import debounce from '@f/debounce'
+
+const typesToCheck = [addCode.type, removeLine.type, updateLine.type, aceUpdate.type]
+
+export default ({getState, dispatch}) => { 
+	const debounced = debounce(() => save(getState, dispatch), 5000)
+	return (next) => (action) => {
+		if (typesToCheck.indexOf(action.type) > -1) {
+			const result = next(action)
+			debounced()
+			return result
+		}
+		return next(action)
+	}
+}
+
+function save (getState, dispatch) {
+	const {saveID, gameID, game} = getState()
+	const {animals} = game
+	if (saveID) {
+		dispatch(saveProgress(animals, gameID, saveID))
+	}
+}
