80889fc0818df2cdf583684a61488ff899607a7b
Andrew Shaffer <andrew@weo.io>
2017-07-05 12:13:15 -0700

    wip

--- functions/lib/playlistUpdateExternal.js
+++ functions/lib/playlistUpdateExternal.js
@@ -4,6 +4,13 @@
 
 const functions = require('firebase-functions')
 const fetch = require('isomorphic-fetch')
+const admin = require('firebase-admin')
+
+/**
+ * Constants
+ */
+
+const playlistsRef = admin.database().ref('/playlists')
 
 /**
  * playlistUpdateExternal
@@ -15,23 +22,41 @@ module.exports = functions.database
   .ref('/playlistInstances/{instRef}/completedChallenges')
   .onWrite(evt => {
     const completed = evt.data.val() || []
+    const {instRef} = evt.params
 
     return evt.data.ref.parent.once('value')
       .then(snap => snap.val())
-      .then(playlist => {
-        const {update} = playlist
-        const body = completed.reduce((acc, id) => {
-          acc[id] = true
-          return acc
-        }, {})
+      .then(playlistInstance => {
+        const {id, playlist: playlistRef, update} = playlistInstance
 
-        console.log('playlistUpdateExternal', completed, playlist, update, body)
+        return playlistsRef
+          .child(playlistRef)
+          .once('value')
+          .then(snap => snap.val())
+          .then(playlist => {
+            const {sequence = []} = playlist
+            const body = completed.reduce((acc, id) => {
+              const idx = sequence.indexOf(id)
 
-        if (update) {
-          return fetch(update, {
-            method: 'POST',
-            body
+              acc[id] = {
+                complete: true,
+                url: `/playlist/${playlistRef}/play/${instRef}/play/${idx}/results`
+              }
+              return acc
+            }, {})
+
+            console.log('playlistUpdateExternal', completed, playlist, update, body)
+
+            if (update) {
+              return fetch(update, {
+                method: 'POST',
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json;charset=UTF-8'
+                },
+                body: JSON.stringify(body)
+              })
+            }
           })
-        }
       })
   })
