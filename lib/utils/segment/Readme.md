c03cd56f0c9ad2782e7caaf5d155a9faf47caed0
danleavitt0 <danleavitt0@gmail.com>
2017-07-10 12:50:04 -0700

    add tracking

---
+++ lib/utils/segment/Readme.md
@@ -0,0 +1,6 @@
+# <Segment/>
+
+
+
+## Usage
+
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var segmentKey = require('./segmentKey')
 var webpack = require('webpack')
 var path = require('path')
 var net = require('net')
@@ -64,7 +65,8 @@ function config (env) {
       }),
       new webpack.DefinePlugin({
         'process.env': {
-          'NODE_ENV': '"production"'
+          'NODE_ENV': '"production"',
+          'TRACKING_CODE': segmentKey
         }
       }),
       new HtmlWebpackPlugin({
