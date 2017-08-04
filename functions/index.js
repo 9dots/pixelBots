608d30ea1c8d1a8c673cec81a231eb014febcb32
danleavitt0 <danleavitt0@gmail.com>
2017-08-04 14:21:31 -0700

    join fixes and other things

--- functions/index.js
+++ functions/index.js
@@ -15,8 +15,3 @@ forEach((f, key) => exports[key] = f, require('./lib'))
 // exports.helloWorld = functions.https.onRequest((req, res) => {
 //   res.send('Hello from Firebase!')
 // })
-
-// exports.likedToMeta = functions.database.ref('/saved/{saveID}/likedBy')
-// 	.onWrite(evt => {
-// 	  return evt.data.ref.parent.child('meta').child('likedBy').set(evt.data.val())
-// })
