1f900cfd895bf0adb736b6679ede7f6710bbb26a
Andrew Shaffer <andrew@weo.io>
2017-08-22 16:55:41 -0700

    fix assignRecentOnJoin

--- functions/lib/assignRecentOnJoin.js
+++ functions/lib/assignRecentOnJoin.js
@@ -33,7 +33,7 @@ module.exports = functions.database.ref('/users/{userRef}/studentOf/{classRef}')
     .limitToFirst(3)
     .once('value')
     .then(snap => snap.val())
-    .then(items => Promise.all([
+    .then((items = {}) => Promise.all([
       Object
         .keys(items)
         .map(key => playlistByUserRef
