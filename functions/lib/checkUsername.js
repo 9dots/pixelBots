86ba34047b0c8a5f0271b8820aa113cdbd97251b
Daniel Guo <danielg216@gmail.com>
2017-11-09 19:24:52 -0800

    cloud functions

---
+++ functions/lib/checkUsername.js
@@ -0,0 +1,42 @@
+/**
+ * Imports
+ */
+
+const admin = require('firebase-admin')
+
+const usersRef = admin.database().ref('/users')
+const usernameRef = admin.database().ref('/usernames')
+
+/**
+ * Retrieve and return a login token
+ */
+
+module.exports = (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(500).send({ status: 'failed', payload: 'Can Only Post' })
+  }
+  const {username} = req.body
+  
+    usernameRef
+    .child(username)
+    .once('value')
+    .then(snap => snap.val())
+    .then(userRef => {
+      if (userRef) {
+        return usersRef
+          .child(userRef)
+          .once('value')
+          .then(user => res.status(200).send({
+            status: 'success',
+            payload: Object.assign({}, user, {uid: userRef})
+          }))
+      } 
+      return Promise.reject({field: 'username', message: 'Not an existing student.'})
+    })
+    .catch(e => {
+      if (e.code === 'auth/user-not-found') {
+        return res.status(200).send({ status: 'success' })
+      }
+      res.status(200).send({ status: 'failed', payload: e })
+    })
+  }
