dadea06dc24a3409eaad4659c74677fa0026a5d8
danleavitt0 <danleavitt0@gmail.com>
2017-11-13 10:24:03 -0800

    format

--- functions/lib/checkUsername.js
+++ functions/lib/checkUsername.js
@@ -15,9 +15,9 @@ module.exports = (req, res) => {
   if (req.method !== 'POST') {
     return res.status(500).send({ status: 'failed', payload: 'Can Only Post' })
   }
-  const {username} = req.body
-  
-    usernameRef
+  const { username } = req.body
+
+  usernameRef
     .child(username)
     .once('value')
     .then(snap => snap.val())
@@ -26,12 +26,17 @@ module.exports = (req, res) => {
         return usersRef
           .child(userRef)
           .once('value')
-          .then(user => res.status(200).send({
-            status: 'success',
-            payload: Object.assign({}, user, {uid: userRef})
-          }))
-      } 
-      return Promise.reject({field: 'username', message: 'Not an existing student.'})
+          .then(user =>
+            res.status(200).send({
+              status: 'success',
+              payload: Object.assign({}, user, { uid: userRef })
+            })
+          )
+      }
+      return Promise.reject({
+        field: 'username',
+        message: 'Not an existing student.'
+      })
     })
     .catch(e => {
       if (e.code === 'auth/user-not-found') {
@@ -39,4 +44,4 @@ module.exports = (req, res) => {
       }
       res.status(200).send({ status: 'failed', payload: e })
     })
-  }
+}
