c1dd307a658080ac09bba0ad69cdabca5b14fc7b
danleavitt0 <danleavitt0@gmail.com>
2018-06-25 16:11:20 -0700

    fix updatehash

--- updateHash.js
+++ updateHash.js
@@ -4,13 +4,12 @@ const fs = require('fs-extra')
 const path = require('path')
 
 const ref = process.env.VERSION === 'V1' ? '/v1Version' : '/versionNumber'
-console.log(ref)
 firebase.initializeApp(config)
 const versionRef = firebase.database().ref(ref)
 
 fs
   .readFile(path.resolve(__dirname, 'public', 'index.html'), 'utf8')
-  .then(html => html.match(/(?:[a-zA-Z1-9]*)(?=\.app)/))
+  .then(html => html.match(/(?:[a-zA-Z0-9]*)(?=\.app)/))
   .then(match => (match ? versionRef.set(match[0]) : Promise.resolve()))
   .then(() => firebase.app().delete())
   .then(() => console.log('done'))
